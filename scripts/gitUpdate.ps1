2c0f38f813ba2a63e0bd4bd0de761cd3473ad663
GitHub Action <github-action@example.com>
2023-04-01 18:03:09 +0000

    Update versioning scripts

--- scripts/gitUpdate.ps1
+++ scripts/gitUpdate.ps1
@@ -1,21 +1,43 @@
-# Define the branch and commit message
-$branch = $args[0]
-$commitMessage = $args[1]
+# Read the configuration file
+$config = Get-Content "config.txt" -Raw | ConvertFrom-Json
+$env = Get-Content ".env" -Raw | ConvertFrom-Json
+
+# Define your GitHub repository information
+$username = $env.GITHUB_USERNAME
+$repoName = $config.repoName
+
+
+# Find the root directory of the repository
+$repoRoot = git rev-parse --show-toplevel
+
+# Get the branch, commit message, username, and token
+$branch = Read-Host "Enter the branch name"
+$commitMessage = Read-Host "Enter the commit description"
+
+# Set the Git credential for the repository
+git -C $repoRoot remote set-url  "https://github.com/$username/$repoName.git"
 
 # Ensure that we are on the correct branch
-git checkout $branch
+git -C $repoRoot checkout $branch
 
-# Add all changes to the Git staging area
-git add .
+# Check if there are any changes in the working directory
+$changes = git -C $repoRoot status --porcelain
 
-# Commit the changes
-git commit -m $commitMessage
+if (!$changes) {
+    Write-Host "No changes detected in the working directory. Skipping the commit."
+} else {
+    # Add all changes to the Git staging area
+    git -C $repoRoot add .
+
+    # Commit the changes
+    git -C $repoRoot commit -m $commitMessage
+}
 
 # Fetch the latest changes from the remote branch
-git fetch origin $branch
+git -C $repoRoot fetch origin $branch
 
 # Check if there are any conflicts between the local and remote branches
-$conflicts = git diff --name-only --diff-filter=U
+$conflicts = git -C $repoRoot diff --name-only --diff-filter=U
 
 if ($conflicts) {
     Write-Host "There are merge conflicts. Please resolve them manually and try again."
@@ -23,10 +45,10 @@ if ($conflicts) {
 }
 
 # Merge the latest changes from the remote branch
-git merge origin/$branch
+git -C $repoRoot merge origin/$branch
 
 # Push the changes to the remote branch
-git push origin $branch
+git -C $repoRoot push origin $branch
 
 # Output a success message
 Write-Host "Project updated and committed to branch $branch"
